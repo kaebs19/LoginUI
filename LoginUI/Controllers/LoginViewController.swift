6013467c26b23688af23fab9ffe58e991d214454
m.saplel <kaebs19@gmail.com>
2023-09-16 05:44:07 +0300

    1.2
    1 - add validate email and password
    2 - show alert
    3 - menager keyboard by TPKeyboardAvoidingSwift

--- LoginUI/Controllers/LoginViewController.swift
+++ LoginUI/Controllers/LoginViewController.swift
@@ -8,26 +8,77 @@
 import UIKit
 
 class LoginViewController: UITableViewController {
-
+    
     // MARK: - Outlets
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // function end keyboard
+        dismissKeyboard()
         
         
-
+        
     }
+    
+    
+    // MARK: -  Configure Action - Func
+    
+   
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        ValidationCode()
+        
+    }
+    
+    
+    @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    // validation email and password
+    fileprivate func ValidationCode() {
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            if !email.validateEmailId() {
+                openAlert(title: "Alert", message: "Email address not found", alertStyle: .alert, actionTitles: ["OK"], actionStyles: [.default], actions: [{_ in
+                    print("Done pressed")
+                }])
+            } else if !password.validatePassword() {
+                openAlert(title: "Alert", message: "please enter valid password", alertStyle: .alert, actionTitles: ["OK"], actionStyles: [.default], actions: [{_ in
+                    
+                    print("The password is right")
+                }])
+            } else {
+                // GO to view home ->
+                
+            }
+            
+        } else {
+            openAlert(title: "Alert", message: "Please add details", alertStyle: .alert, actionTitles: ["OK"], actionStyles: [.default], actions: [{_ in
+                
+                print("Done pressed")
+                
+            }])
+            
+        }
+    }
+}
 
+// MARK: -  TableView Delegate & DataSource
+extension LoginViewController {
+    
+    // center table view in viewContloer
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         let tableViewHeight = self.tableView.frame.height
         let contentHeight = self.tableView.contentSize.height
-        
         
         let centeringInset = (tableViewHeight - contentHeight) / 2.0
         let topInset = max(centeringInset, 0.0)
@@ -39,10 +90,4 @@ class LoginViewController: UITableViewController {
         return UIScreen.main.bounds.height
     }
     
-    // MARK: -  Configure Action - Func
-    
-    
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LoginUI' do
+  use_frameworks!
+pod "TPKeyboardAvoidingSwift"
+
+
+
+
+end
